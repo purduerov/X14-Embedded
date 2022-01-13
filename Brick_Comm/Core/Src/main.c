565e6a7ee4b8af9e9215023e7e421a7e895df677
Bryce Sasser <sasserb@purdue.edu>
2022-01-12 19:49:50 -0500

    The Brick and Solenoid code is combined and working

--- Brick_Comm/Core/Src/main.c
+++ Brick_Comm/Core/Src/main.c
@@ -69,6 +69,20 @@ typedef struct
 #define I2C_BRICK_1_DEVICE_ADDRESS 0x23
 #define I2C_TRANSACTION_TIMEOUT_MS 10000
 
+#define SOLENOID_1_BITSHIFT 0
+#define SOLENOID_2_BITSHIFT 1
+#define SOLENOID_3_BITSHIFT 2
+#define SOLENOID_4_BITSHIFT 3
+#define SOLENOID_5_BITSHIFT 4
+#define SOLENOID_6_BITSHIFT 5
+
+#define SOLENOID_1_BITMASK (0x01 << SOLENOID_1_BITSHIFT)
+#define SOLENOID_2_BITMASK (0x01 << SOLENOID_2_BITSHIFT)
+#define SOLENOID_3_BITMASK (0x01 << SOLENOID_3_BITSHIFT)
+#define SOLENOID_4_BITMASK (0x01 << SOLENOID_4_BITSHIFT)
+#define SOLENOID_5_BITMASK (0x01 << SOLENOID_5_BITSHIFT)
+#define SOLENOID_6_BITMASK (0x01 << SOLENOID_6_BITSHIFT)
+
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -452,6 +466,18 @@ void CAN_FIFO0_RXMessagePendingCallback(CAN_HandleTypeDef *_hcan)
 
     //  Release Output Mailbox
     SET_BIT(_hcan->Instance->RF0R, CAN_RF0R_RFOM0);
+
+    uint8_t solenoidControlByte;
+
+    solenoidControlByte = (uint8_t)((CAN_RDH0R_DATA7 & _hcan->Instance->sFIFOMailBox[0].RDHR) >> CAN_RDH0R_DATA7_Pos);
+    //SET_BIT(_hcan->Instance->RF0R, CAN_RF0R_RFOM0);
+
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, (solenoidControlByte & SOLENOID_1_BITMASK) >> SOLENOID_1_BITSHIFT);
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, (solenoidControlByte & SOLENOID_2_BITMASK) >> SOLENOID_2_BITSHIFT);
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, (solenoidControlByte & SOLENOID_3_BITMASK) >> SOLENOID_3_BITSHIFT);
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, (solenoidControlByte & SOLENOID_4_BITMASK) >> SOLENOID_4_BITSHIFT);
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, (solenoidControlByte & SOLENOID_5_BITMASK) >> SOLENOID_5_BITSHIFT);
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, (solenoidControlByte & SOLENOID_6_BITMASK) >> SOLENOID_6_BITSHIFT);
 }
 
 //  Handles ONLY to reception of power brick communication packets
